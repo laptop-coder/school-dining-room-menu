297aa3ff8faa0adb5497e17a3288e45256823d56
laptop-coder <coderlaptop@gmail.com>
2025-09-06 23:38:39 +0300

    feat(backend): add handler to the main file

--- backend/main.go
+++ backend/main.go
@@ -12,6 +12,7 @@ func main() {
 	defer DB.Close()
 
 	http.HandleFunc("/category/add", handlers.AddCategory)
+	http.HandleFunc("/category/delete", handlers.DeleteCategory)
 	http.HandleFunc("/categories/get_list", handlers.GetCategoriesList)
 	http.HandleFunc("/dish/add", handlers.AddDish)
 	http.HandleFunc("/dish/change_availability", handlers.ChangeDishAvailability)
