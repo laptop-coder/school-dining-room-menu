13108bee7684248d90e65604e31dbd8f6ebf1cd6
laptop-coder <coderlaptop@gmail.com>
2025-09-06 19:23:34 +0300

    feat(backend): add handler to add new dishes to db

---
+++ backend/handlers/add_dish.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	. "backend/database"
+	. "backend/logger"
+	. "backend/utils"
+	"fmt"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+func AddDish(w http.ResponseWriter, r *http.Request) {
+	SetupCORS(&w)
+	if r.Method != http.MethodPost {
+		msg := "A POST request is required"
+		Logger.Warn(msg)
+		http.Error(w, msg, http.StatusMethodNotAllowed)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		msg := "Error. Can't parse form: " + err.Error()
+		Logger.Error(msg)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+	dishName := r.FormValue("dishName")
+	dishCategory := r.FormValue("dishCategory")
+	dishDescription := r.FormValue("dishDescription")
+	sqlQuery := fmt.Sprintf(`
+	INSERT INTO dish (
+		dish_id,
+		dish_category,
+		dish_name,
+		dish_description
+	) VALUES (
+		'%s',
+		'%s',
+		'%s',
+		'%s'
+	);
+	`, uuid.New(), dishName, dishCategory, dishDescription)
+	if _, err := DB.Exec(sqlQuery); err != nil {
+		msg := "Error adding new dish: " + err.Error()
+		Logger.Error(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	msg := "Success. Added new dish"
+	Logger.Info(msg)
+	w.Write([]byte(msg))
+}
