4c6de03fc9eb2b16a7c19569fe379e9e7e827908
laptop-coder <coderlaptop@gmail.com>
2025-09-06 20:58:57 +0300

    feat(backend): add handler to get dishes lists

--- backend/handlers/get_dishes_list.go
+++ backend/handlers/get_dishes_list.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	. "backend/database"
 	. "backend/logger"
+	"backend/types"
 	. "backend/utils"
+	"encoding/json"
 	"net/http"
 )
 
@@ -14,5 +17,44 @@ func GetDishesList(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, msg, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Dishes list"))
+	w.Header().Set("Content-Type", "application/json")
+
+	// Get data from the database
+
+	sqlQuery := "SELECT * FROM dish ORDER BY dish_name;"
+	if rows, err := DB.Query(sqlQuery); err != nil {
+		msg := "Error getting dishes list from the database: " + err.Error()
+		Logger.Error(msg)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	} else {
+		Logger.Info("Success. Received dishes list")
+		// Serialize data and send it in response
+		var dishesList []types.Dish
+		var dish types.Dish
+		for rows.Next() {
+			if err := rows.Scan(
+				&dish.DishId,
+				&dish.DishCategory,
+				&dish.DishName,
+				&dish.DishDescription,
+				&dish.DishAvailable,
+			); err != nil {
+				msg := "Error (\"dish\" object): " + err.Error()
+				Logger.Error(msg)
+				http.Error(w, msg, http.StatusInternalServerError)
+				return
+			}
+			dishesList = append(dishesList, dish)
+		}
+		jsonData, err := json.Marshal(dishesList)
+		if err != nil {
+			msg := "JSON serialization error: " + err.Error()
+			Logger.Error(msg)
+			http.Error(w, msg, http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonData)
+		return
+	}
 }
---
+++ backend/types/dish.go
@@ -0,0 +1,9 @@
+package types
+
+type Dish struct {
+	DishId          string
+	DishCategory    string
+	DishName        string
+	DishDescription string
+	DishAvailable   int
+}
