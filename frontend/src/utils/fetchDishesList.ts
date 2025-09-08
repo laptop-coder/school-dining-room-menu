2444c6249ee0728c13e5dc5c1fec62152044e79f
laptop-coder <coderlaptop@gmail.com>
2025-09-09 00:14:57 +0300

    feat(frontend): add utility to fetch dishes list

---
+++ frontend/src/utils/fetchDishesList.ts
@@ -0,0 +1,16 @@
+import axiosInstance from '../utils/axiosInstance';
+
+const fetchDishesList = async (props: {
+  dishesCategory: string;
+  dishesAvailable: string;
+}) =>
+  axiosInstance
+    .get(
+      `/dishes/get_list?category=${props.dishesCategory}&available=${props.dishesAvailable}`,
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+
+export default fetchDishesList;
