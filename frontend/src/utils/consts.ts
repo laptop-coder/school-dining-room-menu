d7086ce46f108d6f26419e4b2eee9ecd6d0ed963
laptop-coder <coderlaptop@gmail.com>
2025-09-09 00:11:54 +0300

    feat(frontend): add routes and a URL

--- frontend/src/utils/consts.ts
+++ frontend/src/utils/consts.ts
@@ -1,1 +1,5 @@
+export const ADMIN_ROUTE = '/1b75e3c7-4c17-4c5a-a701-e13953a38ad4'; // TODO: move to the .env file
 export const HOME_ROUTE = '/';
+
+export const ASSETS_ROUTE = '/storage/assets';
+export const BACKEND_URL = 'https://server.school-dining-room-menu.ru';
