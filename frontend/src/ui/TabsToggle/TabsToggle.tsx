4f10caa2f4ff1a191a774f7de683c456a8a74c2c
laptop-coder <coderlaptop@gmail.com>
2025-09-09 00:15:51 +0300

    feat(frontend): add tabs toggle (JSX element)

---
+++ frontend/src/ui/TabsToggle/TabsToggle.tsx
@@ -0,0 +1,53 @@
+import { JSX, createSignal, onMount, For, createEffect } from 'solid-js';
+import type { Signal, Setter } from 'solid-js';
+
+import styles from './TabsToggle.module.css';
+
+import { Motion } from 'solid-motionone';
+
+const TabsToggle = (props: {
+  tabs: string[];
+  setter: Setter<any>;
+}): JSX.Element => {
+  const [activeTab, setActiveTab] = createSignal(0);
+  const [activeTabInfo, setActiveTabInfo]: Signal<{
+    left: number;
+    width: string;
+  }> = createSignal({ left: 0, width: '0px' });
+  const tabsRefs: HTMLButtonElement[] = [];
+  onMount(() => {
+    createEffect(() => {
+      const rect = tabsRefs[activeTab()].getBoundingClientRect();
+      setActiveTabInfo({ left: rect.left, width: String(rect.width) + 'px' });
+    });
+  });
+  props.setter(props.tabs[0]);
+  return (
+    <div class={styles.tabs_toggle}>
+      <div class={styles.tabs}>
+        <For each={props.tabs}>
+          {(tab, index) => (
+            <button
+              ref={(el) => (tabsRefs[index()] = el)}
+              onClick={() => {
+                setActiveTab(index());
+                props.setter(props.tabs[index()]);
+              }}
+              class={styles.tab}
+            >
+              <span class={styles.tab_text}>{tab}</span>
+            </button>
+          )}
+        </For>
+      </div>
+      <Motion
+        initial={false}
+        animate={{ x: activeTabInfo().left, width: activeTabInfo().width }}
+        transition={{ duration: 0.3 }}
+        class={styles.pointer}
+      />
+    </div>
+  );
+};
+
+export default TabsToggle;
