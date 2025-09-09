5d1ce7d25158b16b07ce3360c94aa7546240733f
laptop-coder <coderlaptop@gmail.com>
2025-09-09 21:29:32 +0300

    chore(frontend): add storage route

--- frontend/src/utils/consts.ts
+++ frontend/src/utils/consts.ts
@@ -2,4 +2,5 @@ export const ADMIN_ROUTE = '/1b75e3c7-4c17-4c5a-a701-e13953a38ad4'; // TODO: mov
 export const HOME_ROUTE = '/';
 
 export const ASSETS_ROUTE = '/storage/assets';
+export const STORAGE_ROUTE = '/storage/storage';
 export const BACKEND_URL = 'https://server.school-dining-room-menu.ru';
