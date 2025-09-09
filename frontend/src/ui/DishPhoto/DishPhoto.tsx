5333ddca7a20b4f97c6f3c3288019c26148dae51
laptop-coder <coderlaptop@gmail.com>
2025-09-09 21:26:45 +0300

    feat(frontend): add dish photo (JSX element)

---
+++ frontend/src/ui/DishPhoto/DishPhoto.tsx
@@ -0,0 +1,18 @@
+import { JSX, Show } from 'solid-js';
+
+import styles from './DishPhoto.module.css';
+
+const DishPhoto = (
+  props: JSX.ImgHTMLAttributes<HTMLImageElement>,
+): JSX.Element => (
+  <Show when={props.src}>
+    <img
+      src={props.src}
+      title={props.title}
+      class={styles.dish_photo}
+      onclick={(event) => event.target.requestFullscreen()}
+    />
+  </Show>
+);
+
+export default DishPhoto;
