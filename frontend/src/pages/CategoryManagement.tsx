a88a546894c00d6b048a85fa64ad4a3b14c81f41
laptop-coder <coderlaptop@gmail.com>
2025-09-09 23:32:46 +0300

    feat(frontend): category management page template

---
+++ frontend/src/pages/CategoryManagement.tsx
@@ -0,0 +1,19 @@
+import { JSX } from 'solid-js';
+
+import Page from '../ui/Page/Page';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Content from '../components/Content/Content';
+
+const CategoryManagementPage = (): JSX.Element => {
+  return (
+    <Page>
+      <Header admin />
+      <Content>
+      </Content>
+      <Footer />
+    </Page>
+  );
+};
+
+export default CategoryManagementPage;
