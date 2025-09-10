ad71b0d82fd92edb20e136a47eb0cf30eb6d0365
laptop-coder <coderlaptop@gmail.com>
2025-09-10 23:47:13 +0300

    feat(frontend): `AdminCategoriesListItem` (JSX)

---
+++ frontend/src/ui/AdminCategoriesListItem/AdminCategoriesListItem.tsx
@@ -0,0 +1,27 @@
+import { JSX } from 'solid-js';
+
+import styles from './AdminCategoriesListItem.module.css';
+import deleteCategory from '../../utils/deleteCategory';
+
+const AdminCategoriesListItem = (props: {
+  categoryName: string;
+  reloadCategoriesList: () => {};
+}): JSX.Element => {
+  return (
+    <div
+      class={styles.admin_categories_list_item}
+      onclick={() => {
+        if (confirm(`Подтвердите удаление категории "${props.categoryName}"`)) {
+          deleteCategory({
+            categoryName: props.categoryName,
+            reloadCategoriesList: props.reloadCategoriesList,
+          });
+        }
+      }}
+    >
+      {props.categoryName}
+    </div>
+  );
+};
+
+export default AdminCategoriesListItem;
