05d1c23eb3626cd748ddc60d39f3eecafa2c18fa
laptop-coder <coderlaptop@gmail.com>
2025-09-11 00:02:54 +0300

    feat(frontend): add content (category management)
    Add content to the category management page.

--- frontend/src/pages/CategoryManagement.tsx
+++ frontend/src/pages/CategoryManagement.tsx
@@ -4,12 +4,14 @@ import Page from '../ui/Page/Page';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Content from '../components/Content/Content';
+import AdminCategoriesList from '../components/AdminCategoriesList/AdminCategoriesList';
 
 const CategoryManagementPage = (): JSX.Element => {
   return (
     <Page>
       <Header admin />
       <Content>
+        <AdminCategoriesList />
       </Content>
       <Footer />
     </Page>
