41431a0e05f973a361bc1e709a7b5003f40f695d
laptop-coder <coderlaptop@gmail.com>
2025-09-10 23:52:33 +0300

    feat(frontend): form to add categories (JSX elem)

---
+++ frontend/src/components/AddCategoryForm.tsx
@@ -0,0 +1,25 @@
+import { JSX, createSignal } from 'solid-js';
+import Input from '../ui/Input/Input';
+import SubmitButton from '../ui/SubmitButton/SubmitButton';
+import Form from '../ui/Form/Form';
+import addCategory from '../utils/addCategory';
+
+const AddCategoryForm = (): JSX.Element => {
+  const [categoryName, setCategoryName] = createSignal('');
+  const handleSubmit = () => {
+    addCategory({ categoryName: categoryName() });
+  };
+  return (
+    <Form onsubmit={handleSubmit}>
+      <Input
+        placeholder='Название категории'
+        name='category_name'
+        value={categoryName()}
+        oninput={(event) => setCategoryName(event.target.value)}
+      />
+      <SubmitButton name='add_category_submit'>Добавить категорию</SubmitButton>
+    </Form>
+  );
+};
+
+export default AddCategoryForm;
