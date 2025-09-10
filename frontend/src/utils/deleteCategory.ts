3af84258c0f3c8983eff3d659c20f8604f826c85
laptop-coder <coderlaptop@gmail.com>
2025-09-10 23:49:41 +0300

    feat(frontend): utility to delete categories

---
+++ frontend/src/utils/deleteCategory.ts
@@ -0,0 +1,23 @@
+import axiosInstance from './axiosInstance';
+
+const deleteCategory = async (props: {
+  categoryName: string;
+  reloadCategoriesList: () => {};
+}) =>
+  axiosInstance
+    .post(
+      `/category/delete`,
+      { categoryName: props.categoryName },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    .then((response) => {
+      props.reloadCategoriesList();
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+
+export default deleteCategory;
