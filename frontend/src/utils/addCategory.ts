e0eda2df9883814e9b6c1acb99746283efd11c34
laptop-coder <coderlaptop@gmail.com>
2025-09-10 23:49:26 +0300

    feat(frontend): utility to add categories

---
+++ frontend/src/utils/addCategory.ts
@@ -0,0 +1,21 @@
+import axiosInstance from './axiosInstance';
+import { CATEGORY_MANAGEMENT_ROUTE } from './consts';
+
+const addCategory = async (props: { categoryName: string }) =>
+  axiosInstance
+    .post(
+      `/category/add`,
+      { categoryName: props.categoryName },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    .then((response) => {
+      window.location.href = CATEGORY_MANAGEMENT_ROUTE;
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+
+export default addCategory;
