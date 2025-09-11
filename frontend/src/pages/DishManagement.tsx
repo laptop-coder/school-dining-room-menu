07e39b6ee70037318b021be8a40b6075e3b7109f
laptop-coder <coderlaptop@gmail.com>
2025-09-12 01:49:19 +0300

    feat(frontend): add elems to dish management page

--- frontend/src/pages/DishManagement.tsx
+++ frontend/src/pages/DishManagement.tsx
@@ -1,15 +1,20 @@
-import { JSX } from 'solid-js';
+import { JSX, createSignal } from 'solid-js';
 
 import Page from '../ui/Page/Page';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Content from '../components/Content/Content';
+import AdminDishesList from '../components/AdminDishesList/AdminDishesList';
+import CategoryToggle from '../components/CategoryToggle';
 
 const DishManagementPage = (): JSX.Element => {
+  const [category, setCategory] = createSignal('');
   return (
     <Page>
       <Header admin />
       <Content>
+        <CategoryToggle setter={setCategory} />
+        <AdminDishesList category={category} />
       </Content>
       <Footer />
     </Page>
