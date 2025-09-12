490e78145fe4315bb75d1cd415fba0850f405f2d
laptop-coder <coderlaptop@gmail.com>
2025-09-12 10:44:22 +0300

    feat(frontend): dish availability management page

--- frontend/src/pages/DishAvailabilityManagement.tsx
+++ frontend/src/pages/DishAvailabilityManagement.tsx
@@ -1,15 +1,21 @@
-import { JSX } from 'solid-js';
+import { JSX, createSignal } from 'solid-js';
 
 import Page from '../ui/Page/Page';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Content from '../components/Content/Content';
+import DishAvailabilityList from '../components/DishAvailabilityList/DishAvailabilityList';
+import CategoryToggle from '../components/CategoryToggle';
 
 const DishAvailabilityManagementPage = (): JSX.Element => {
+  const [category, setCategory] = createSignal('');
   return (
     <Page>
       <Header admin />
-      <Content></Content>
+      <Content>
+        <CategoryToggle setter={setCategory} />
+        <DishAvailabilityList category={category} />
+      </Content>
       <Footer />
     </Page>
   );
