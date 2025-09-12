9c564867aedf834205d36f536ca32947c56dd420
laptop-coder <coderlaptop@gmail.com>
2025-09-12 10:32:23 +0300

    feat(frontend): change dishes availability utility

---
+++ frontend/src/utils/changeDishAvailability.ts
@@ -0,0 +1,27 @@
+import axiosInstance from './axiosInstance';
+
+const changeDishAvailability = async (props: {
+  dishId: string;
+  newAvailabilityValue: '0' | '1';
+  reloadDishesList: Function;
+}) =>
+  axiosInstance
+    .post(
+      `/dish/change_availability`,
+      {
+        dishId: props.dishId,
+        newAvailabilityValue: props.newAvailabilityValue,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    .then((response) => {
+      props.reloadDishesList();
+      return response.data;
+    })
+    .catch((error) => console.log(error));
+
+export default changeDishAvailability;
