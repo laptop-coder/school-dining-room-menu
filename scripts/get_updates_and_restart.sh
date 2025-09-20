0824d985d77902abb03a3bd181724e2c619e47ff
laptop-coder <coderlaptop@gmail.com>
2025-09-20 10:59:19 +0300

    ci: get updates and restart (`systemd` and script)
    `Systemd` unit and script to get updates and restart

---
+++ scripts/get_updates_and_restart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+BACKEND_OLD_IMAGE_ID="$(docker inspect laptopcoder/school-dining-room-menu-backend --format {{.Id}})"
+FRONTEND_OLD_IMAGE_ID="$(docker inspect laptopcoder/school-dining-room-menu-frontend --format {{.Id}})"
+docker pull laptopcoder/school-dining-room-menu-backend:latest > /dev/null
+docker pull laptopcoder/school-dining-room-menu-frontend:latest  > /dev/null
+BACKEND_NEW_IMAGE_ID="$(docker inspect laptopcoder/school-dining-room-menu-backend --format {{.Id}})"
+FRONTEND_NEW_IMAGE_ID="$(docker inspect laptopcoder/school-dining-room-menu-frontend --format {{.Id}})"
+
+
+if [ "$BACKEND_OLD_IMAGE_ID" != "$BACKEND_NEW_IMAGE_ID" ] || [ "$FRONTEND_OLD_IMAGE_ID" != "$FRONTEND_NEW_IMAGE_ID" ]; then
+    echo "The image(s) on the Docker Hub was(were) updated, restarting the project..."
+    docker restart backend-prod
+    docker restart frontend-prod
+else
+    echo "The images on the Docker Hub were not updated."
+fi;
