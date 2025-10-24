6e9316116a74ddf4f032bd3705f36d0f847d305e
laptop-coder <coderlaptop@gmail.com>
2025-10-24 22:52:05 +0300

    refactor: set up Docker network

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# set the default value of the FRONTEND_PORT if empty
+if [ "$FRONTEND_PORT" = '' ]; then
+    export FRONTEND_PORT=29157
+fi
+
 sed -i '/^#/d' nginx.conf.template # remove comments
 sed -i 's/\$\$\$/###/g' nginx.conf.template # replace $$$ by ### (screening)
 envsubst < nginx.conf.template > nginx.conf # substitute env variables
