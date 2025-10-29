df275d08ac5fa8d0576156f77ac8cc683a313f38
laptop-coder <coderlaptop@gmail.com>
2025-10-29 18:48:06 +0300

    fix(backend): improve regular expressions
    Change groups of symbols in checks of strings.

--- backend/utils/check_string.go
+++ backend/utils/check_string.go
@@ -5,8 +5,15 @@ import (
 	"regexp"
 )
 
+/*
+\p{L} - any letters of any languages
+\p{Z} - any type of space characters or separators
+\p{P} - any punctuation marks
+\p{N} - any type of number marks of any languages
+*/
+
 func IsText(s string) (*bool, error) {
-	result, err := regexp.MatchString("^[a-zA-Z\\p{Cyrillic}\\s]+$", s)
+	result, err := regexp.MatchString("^[\\p{L}\\p{Z}\\p{P}\\t\\r\\n]+$", s)
 	if err != nil {
 		return nil, errors.New("Error checking that string consist only of letters and space/tab/enter symbols: " + err.Error())
 	}
@@ -14,7 +21,7 @@ func IsText(s string) (*bool, error) {
 }
 
 func IsTextOrNumbers(s string) (*bool, error) {
-	result, err := regexp.MatchString("^[a-zA-Z\\p{Cyrillic}0-9\\s]+$", s)
+	result, err := regexp.MatchString("^[\\p{L}\\p{Z}\\p{P}\\p{N}\\t\\r\\n]+$", s)
 	if err != nil {
 		return nil, errors.New("Error checking that string consist only of letters, numbers and space/tab/enter symbols: " + err.Error())
 	}
