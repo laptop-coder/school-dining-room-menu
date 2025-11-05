801578316193291896344498d1f9404d3603d5ca
laptop-coder <coderlaptop@gmail.com>
2025-11-05 14:13:39 +0300

    change `localhost` to the random local IP

--- frontend/src/utils/consts.ts
+++ frontend/src/utils/consts.ts
@@ -15,5 +15,5 @@ export const BACKEND_ADMIN_REGISTER_ROUTE = '/admin/register';
 export const ASSETS_ROUTE = '/storage/assets';
 export const STORAGE_ROUTE = '/storage/storage';
 
-export const BACKEND_URL = 'http://localhost:14536';
+export const BACKEND_URL = 'http://10.0.6.100:14536';
 export const SCHOOL_URL = 'https://лицей369.рф';
