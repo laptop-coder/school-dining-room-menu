b8ef7cd6dc24be071c0fb7d0a57851e21fde7561
laptop-coder <coderlaptop@gmail.com>
2025-11-05 14:25:27 +0300

    fix(backend): CORS

--- backend/utils/setup_cors.go
+++ backend/utils/setup_cors.go
@@ -8,6 +8,6 @@ import (
 func SetupCORS(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:"+Cfg.App.PortFrontend)
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://10.0.6.100:"+Cfg.App.PortFrontend)
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
