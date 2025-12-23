73799d4074785e13a684fbf188bdfb18cf3fbcc2
laptop-coder <coderlaptop@gmail.com>
2025-12-23 12:17:06 +0300

    ci: add docs to the restart script

--- scripts/get_updates_and_restart.sh
+++ scripts/get_updates_and_restart.sh
@@ -4,8 +4,10 @@ docker-compose -f "${HOME}/school-dining-room-menu/compose.yaml" down
 
 docker rmi laptopcoder/school-dining-room-menu-backend:latest > /dev/null
 docker rmi laptopcoder/school-dining-room-menu-frontend:latest  > /dev/null
+docker rmi laptopcoder/school-dining-room-menu-docs:latest  > /dev/null
 
 docker pull laptopcoder/school-dining-room-menu-backend:latest > /dev/null
 docker pull laptopcoder/school-dining-room-menu-frontend:latest  > /dev/null
+docker pull laptopcoder/school-dining-room-menu-docs:latest  > /dev/null
 
 docker-compose -f "${HOME}/school-dining-room-menu/compose.yaml" up -d
